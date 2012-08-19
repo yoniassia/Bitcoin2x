9eb8c28b0f0c6bc9ce6c7f0896acfe9322b7b6c1
Yoni <yoni.assia@gmail.com>
2012-08-19 21:14:34 +0300

    testing this environement with node.js
    testing this environement with node.js

--- web.js
+++ web.js
@@ -6,6 +6,14 @@ app.get('/', function(request, response) {
   response.send('Hello World!');
 });
 
+app.get('/1', function(request, response) {
+  response.send('Hello you!');
+});
+
+
+
+
+
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
